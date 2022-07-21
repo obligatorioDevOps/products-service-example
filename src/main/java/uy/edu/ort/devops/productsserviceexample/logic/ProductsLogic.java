43a8271fbe518699940c4213302d54a5fc9aa26f
Diego Melo <diegomelo520@gmail.com>
2022-07-21 02:39:07 -0300

    New product

--- src/main/java/uy/edu/ort/devops/productsserviceexample/logic/ProductsLogic.java
+++ src/main/java/uy/edu/ort/devops/productsserviceexample/logic/ProductsLogic.java
@@ -19,6 +19,7 @@ public class ProductsLogic {
 
     public ProductsLogic() {
         testProducts = new HashMap<>();
+        testProducts.put("444", new Product("444", "Producto 444", 10, "Este es un producto de diego"));
         testProducts.put("123", new Product("123", "Producto 123", 10, "Este es un producto"));
         testProducts.put("321", new Product("321", "Producto 321", 10, "Este es un producto"));
         testProducts.put("111", new Product("111", "Producto 111", 10, "Este es un producto"));
